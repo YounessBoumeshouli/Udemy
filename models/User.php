9a2bf662fc95778ad9111252cbc00929e867e865
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-01-15 15:35:17 +0100

    constructeur de class User

--- models/User.php
+++ models/User.php
@@ -7,5 +7,12 @@ abstract class user {
     protected $email;
     protected $role;
 
-    
+    public function __construct($db, $userData = null) {
+        $this->db = $db;
+        if ($userData) {
+            $this->id = $userData['id'];
+            $this->username = $userData['username'];
+            $this->email = $userData['email'];
+        }
+    }
 }

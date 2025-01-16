d1ca54ecd90af09ef4be3f3b6e23657a5ade0d24
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-01-16 15:51:21 +0100

    surcharge la methode de create dans la class abstract User

--- models/User.php
+++ models/User.php
@@ -15,4 +15,28 @@ abstract class user {
             $this->email = $userData['email'];
         }
     }
+    public function create($data){
+        echo "<pre>";
+        var_dump($data);
+        var_dump($this->db);
+        echo "</pre>";
+        echo $this->status;
+        $sql = "INSERT INTO public.users(
+	     username, email, password, role, status)
+	    VALUES (:username, :email, :password, :role,:status)";
+        $password = password_hash($data['password'], PASSWORD_DEFAULT);
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':username', $data['username']);
+        $stmt->bindParam(':email', $data['email']);
+        $stmt->bindParam(':status', $data['status']);
+        $stmt->bindParam(':password', $password);
+        $stmt->bindParam(':role', $data['role']);
+        echo $password;
+        try {
+            $stmt->execute();
+            echo "executed";
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }   
+    }
 }

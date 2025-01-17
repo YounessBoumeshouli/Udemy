b30f7ed2930ca8a00efe059d8ea77e1460d6a212
BoumeshouliYouness <boumeshouliyouness6@gmail.com>
2025-01-17 19:33:17 +0100

    constructeur de class course

--- models/Course.php
+++ models/Course.php
@@ -1,5 +1,15 @@
 <?php
 require_once 'CrudInterface.php';
 abstract class Course implements CrudInterface, DisplayableInterface {
+    protected $db;
+    protected $id;
+    protected $title;
+    protected $description;
+    protected $teacher_id;
+    protected $category_id;
+    protected $status;
 
+    public function __construct($db) {
+        $this->db = $db;
+    }
 }
